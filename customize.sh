0d7068de6a3a1f0132468e291c218c80219913f3
shivammanav22 <48555870+shivammanav22@users.noreply.github.com>
2021-12-24 23:14:04 +0530

    update script

--- customize.sh
+++ customize.sh
@@ -1,21 +1,3 @@
-##########################################################################################
-#
-# Unity Config Script
-# by topjohnwu, modified by Zackptg5
-#
-##########################################################################################
-
-##########################################################################################
-# Unity Logic - Don't modify this
-##########################################################################################
-
-SKIPUNZIP=1
-if [ -z $UF ]; then
-  MAGISK=true; UF=$TMPDIR/common/unityfiles
-  unzip -oq "$ZIPFILE" 'common/unityfiles/*' -d $TMPDIR >&2
-  [ -f "$UF/util_functions.sh" ] || { ui_print "! Unable to extract zip file !"; exit 1; }
-  . $UF/util_functions.sh
-fi
 
 ##########################################################################################
 # Config Flags
@@ -23,7 +5,7 @@ fi
 
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard in magisk manager and the zip directory in twrp) - note that this can still be set to true by the user by adding 'debug' to the zipname
+# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
 #MINAPI=21
 #MAXAPI=25
 #DYNLIB=true
@@ -50,39 +32,27 @@ REPLACE="
 "
 
 ##########################################################################################
-# Custom Logic
+# Permissions
 ##########################################################################################
 
 set_permissions() {
   : # Remove this if adding to this function
 
-  # Note that all files/folders have the $UNITY prefix - keep this prefix on all of your files/folders
-  # Also note the lack of '/' between variables - preceding slashes are already included in the variables
-  # Use $VEN for vendor (Do not use /system$VEN, the $VEN is set to proper vendor path already - could be /vendor, /system/vendor, etc.)
-
+  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
   # Some examples:
   
   # For directories (includes files in them):
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
   
-  # set_perm_recursive $UNITY/system/lib 0 0 0755 0644
-  # set_perm_recursive $UNITY$VEN/lib/soundfx 0 0 0755 0644
+  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
+  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
 
   # For files (not in directories taken care of above)
   # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
   
-  # set_perm $UNITY/system/lib/libart.so 0 0 0644
+  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
+  # set_perm /data/local/tmp/file.txt 0 0 644
+
+  set_perm_recursive $MODPATH/system/vendor/lib/egl/ 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/egl/ 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 }
-
-# Custom Variables for Install AND Uninstall - Keep everything within this function - runs before uninstall/install
-unity_custom() {
-  : # Remove this if adding to this function
-}
-
-# Custom Functions for Install AND Uninstall - You can put them here
-
-##########################################################################################
-# Unity Logic - Don't touch anything after this
-##########################################################################################
-
-unity_main
