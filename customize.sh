bd646b6b0438a44d70a25def8230bc002efb05c2
shivammanav22 <48555870+shivammanav22@users.noreply.github.com>
2022-01-01 17:16:45 +0530

    Update script for V3
    update for refresh rate and perf mode

--- customize.sh
+++ customize.sh
@@ -1,3 +1,8 @@
+##########################################################################################
+#
+# MMT Extended Config Script
+#
+##########################################################################################
 
 ##########################################################################################
 # Config Flags
@@ -52,7 +57,45 @@ set_permissions() {
   
   # set_perm $MODPATH/system/lib/libart.so 0 0 0644
   # set_perm /data/local/tmp/file.txt 0 0 644
-
+  set_perm_recursive $MODPATH  0  0  0755  0644
+  set_perm_recursive $MODPATH/system/vendor/bin 0 0 0755 0644 u:object_r:vendor_firmware_file:s0
+  set_perm_recursive $MODPATH/system/vendor/bin/hw 0 0 0755 0644 u:object_r:vendor_firmware_file:s0
+  set_perm_recursive $MODPATH/system/vendor/bin/hw/android.hardware.graphics.composer@2.4-service 0 0 0755 0644 u:object_r:hal_graphics_composer_default_exec:s0
+  set_perm_recursive $MODPATH/system/vendor/firmware 0 0 0755 0644 u:object_r:vendor_firmware_file:s0
+  set_perm_recursive $MODPATH/system/vendor/etc 0 0 0755 0644 u:object_r:vendor_configs_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/ 0 0 0755 0644 u:object_r:vendor_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/libEGL_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/libGLESv2_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/libadreno_app_profiles.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/libq3dtools_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/hw/vulkan.adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/hw/android.hardware.graphics.mapper@3.0-impl-qti-display.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/hw/android.hardware.graphics.mapper@4.0-impl-qti-display.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/hw/gralloc.default.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/hw/gralloc.msmnile.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
   set_perm_recursive $MODPATH/system/vendor/lib/egl/ 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib/egl/egl.cfg 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/ 0 0 0755 0644 u:object_r:vendor_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/libEGL_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/libGLESv2_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/libadreno_app_profiles.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/libq3dtools_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw/vulkan.adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw/android.hardware.graphics.mapper@3.0-impl-qti-display.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw/android.hardware.graphics.mapper@4.0-impl-qti-display.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw/gralloc.default.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw/gralloc.msmnile.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
   set_perm_recursive $MODPATH/system/vendor/lib64/egl/ 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/lib64/egl/egl.cfg 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+  set_perm_recursive $MODPATH/system/vendor/etc/device_features 0 0 0755 0644 u:object_r:vendor_configs_file:s0
+  set_perm_recursive $MODPATH/system/vendor/etc/device_features/bhima.xml 0 0 0755 0644 u:object_r:vendor_configs_file:s0
+  set_perm_recursive $MODPATH/system/vendor/etc/device_features/vayu.xml 0 0 0755 0644 u:object_r:vendor_configs_file:s0
 }
+
+##########################################################################################
+# MMT Extended Logic - Don't modify anything after this
+##########################################################################################
+
+SKIPUNZIP=1
+unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
+. $TMPDIR/functions.sh
